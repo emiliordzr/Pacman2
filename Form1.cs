a4b18939c977a1ba407cd08f3a50c08bee8048f0
emiliordzr <122382723+emiliordzr@users.noreply.github.com>
2023-02-06 19:10:21 -0600

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace pac_man
         Scene scene;
         Player p;
         bool hold, right, left, down = false;
-        bool up = true;
+        bool up = false;
         float distance;
         PointF valid, valid2, valid3;
         public Form1()
@@ -39,22 +39,112 @@ namespace pac_man
         {
             if (left == true && hold == false)
             {
-                pacman.Left -= 1;
-               
+                up = false;
+                down = false;
+                left = true;
+                right = false;
+                p.LeftRight();
+                p.Turn(4);
+                if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
+                {
+                    p.Turn(4);
+                    forward += step;
+                    UpdatePosition();
+                    
+                }
+                if ((p.middle.X - 7.5f <= valid.X + 1 && valid.X != 0 && valid.Y != 0) || (p.middle2.X - 7.5f <= valid2.X + 1 && valid2.X != 0 && valid2.Y != 0) || (p.middle3.X - 7.5f <= valid3.X + 1 && valid3.X != 0 && valid3.Y != 0))
+                {
+                    left = false;
+                    
+                }
+                else
+                {
+                    p.Turn(4);
+                    forward += step;
+                    UpdatePosition();
+                }
+
             }
             else if (right == true && hold == false)
             {
-                pacman.Left += 1;
-               
+                up = false;
+                down = false;
+                left = false;
+                right = true;
+                p.LeftRight();
+                p.Turn(2);
+                if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
+                {
+                    p.Turn(2);
+                    forward += step;
+                    UpdatePosition();
+                }
+                if ((p.middle.X + 7.5f >= valid.X - 1 && valid.X != 0 && valid.Y != 0) || (p.middle2.X + 7.5f >= valid2.X - 1 && valid2.X != 0 && valid2.Y != 0) || (p.middle3.X + 7.5f >= valid3.X - 1 && valid3.X != 0 && valid3.Y != 0))
+                {
+                    right=false;
+                }
+                else
+                {
+                    p.Turn(2);
+                    forward += step;
+                    UpdatePosition();
+                }
+
             }
             else if (up == true && hold == false)
             {
-                pacman.Top -= 1;
+                up = true;
+                down = false;
+                left = false;
+                right = false;
+                p.UpDown();
+                p.Turn(1);
+                if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
+                {
+                    p.Turn(1);
+                    forward += step;
+                    UpdatePosition();
+                }
+                if ((p.middle.Y - 7.5f <= valid.Y + 1 && valid.X != 0 && valid.Y != 0) || (p.middle2.Y - 7.5f <= valid2.Y + 1 && valid2.X != 0 && valid2.Y != 0) || (p.middle3.Y - 7.5f <= valid3.Y + 1 && valid3.X != 0 && valid3.Y != 0))
+                {
+                    up = false;
+                }
+                else
+                {
+                    p.Turn(1);
+                    forward += step;
+                    UpdatePosition();
+                }
             }
             else if (down == true && hold == false)
             {
-                pacman.Top += 1;
+                
+                up = false;
+                down = true;
+                left = false;
+                right = false;
+                p.UpDown();
+                p.Turn(3);
+                if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
+                {
+                    p.Turn(3);
+                    forward += step;
+                    UpdatePosition();
+                    
+                }
+                if ((p.middle.Y + 7.5f >= valid.Y - 1 && valid.X != 0 && valid.Y != 0) || (p.middle2.Y + 7.5f >= valid2.Y - 1 && valid2.X != 0 && valid2.Y != 0) || (p.middle3.Y + 7.5f >= valid3.Y - 1 && valid3.X != 0 && valid3.Y != 0))
+                {
+                    down=false;
+                }
+                else
+                {
+                    p.Turn(3);
+                    forward += step;
+                    UpdatePosition();
+                   
+                }
             }
+            Render();
         }
 
         public void level1()
@@ -119,18 +209,54 @@ namespace pac_man
             map.Invalidate();
         }
 
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Right & !hold)
+            { 
+                pacman.Image = MyResource.fijo;
+            }
+            else if (e.KeyData == Keys.Left & !hold)
+            {
+                pacman.Image = MyResource.fijo;
+            }
+            else if (e.KeyData == Keys.Up & !hold)
+            {
+                pacman.Image = MyResource.fijo;
+            }
+            else if (e.KeyData == Keys.Down & !hold)
+            {
+                pacman.Image = MyResource.fijo;
+            }
+        }
+
         public void DrawPlayer()
         {
             g.FillEllipse(Brushes.Yellow, p.pos.X, p.pos.Y, 15, 15);
-            g.DrawLine(Pens.Red, p.middle, valid);
-            g.DrawLine(Pens.Red, p.middle2, valid2);
-            g.DrawLine(Pens.Red, p.middle3, valid3);
+            //g.DrawLine(Pens.Red, p.middle, valid);
+            //g.DrawLine(Pens.Red, p.middle2, valid2);
+            //g.DrawLine(Pens.Red, p.middle3, valid3);
 
         }
 
         public void UpdatePosition()
         {
             float f = (float)forward / 50;
+            if (p.pos.X > 605)
+            {
+                p.middle.X = -50f;
+            }
+            if (p.pos.X < -20)
+            {
+                p.middle.X = 650f;
+            }
+            if (p.pos.Y > 605)
+            {
+                p.middle.Y = -50f;
+            }
+            if (p.pos.Y < -20)
+            {
+                p.middle.Y = 650f;
+            }
             p.middle = Util.Instance.NextStep(p.middle, p.lookAt, f);
             p.middle2 = Util.Instance.NextStep(p.middle2, p.lookAt2, f);
             p.middle3 = Util.Instance.NextStep(p.middle3, p.lookAt3, f);
@@ -140,116 +266,6 @@ namespace pac_man
             forward = 0;
         }
 
-        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
-        {
-            switch (keyData)
-            {
-                case Keys.Left:
-                    up = false;
-                    down = false;
-                    left = true;
-                    right = false;
-                    p.LeftRight();
-                    p.Turn(4);
-                    if(valid.X==0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
-                    {
-                        p.Turn(4);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                    if (p.middle.X - 7.5f <= valid.X+1 || p.middle2.X - 7.5f <= valid2.X + 1 || p.middle3.X - 7.5f <= valid3.X + 1)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        p.Turn(4);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                case Keys.Right:
-                    up = false;
-                    down = false;
-                    left = false;
-                    right = true;
-                    p.LeftRight();
-                    p.Turn(2);
-                    if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
-                    {
-                        p.Turn(2);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                    if ((p.middle.X + 7.5f >= valid.X - 1 && valid.X!=0 && valid.Y!=0) || (p.middle2.X + 7.5f >= valid2.X - 1 && valid2.X!=0 && valid2.Y!=0) || (p.middle3.X + 7.5f >= valid3.X - 1 && valid3.X!=0 && valid3.Y!=0))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        p.Turn(2);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                case Keys.Up:
-                    up = true;
-                    down = false;
-                    left = false;
-                    right = false;
-                    p.UpDown();
-                    p.Turn(1);
-                    if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
-                    {
-                        p.Turn(1);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                    if (p.middle.Y - 7.5f <= valid.Y + 1 || p.middle2.Y - 7.5f <= valid2.Y + 1 || p.middle3.Y - 7.5f <= valid3.Y + 1)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        p.Turn(1);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    };
-                case Keys.Down:
-                    up = false;
-                    down = true;
-                    left = false;
-                    right = false;
-                    p.UpDown();
-                    p.Turn(3);
-                    if (valid.X == 0 && valid.Y == 0 && valid2.X == 0 && valid2.Y == 0 && valid3.X == 0 && valid3.Y == 0)
-                    {
-                        p.Turn(3);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                    if ((p.middle.Y + 7.5f >= valid.Y - 1 && valid.X != 0 && valid.Y != 0) ||( p.middle2.Y + 7.5f >= valid2.Y - 1 && valid2.X != 0 && valid2.Y != 0) || (p.middle3.Y + 7.5f >= valid3.Y - 1 && valid3.X != 0 && valid3.Y != 0))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        p.Turn(3);
-                        forward += step;
-                        UpdatePosition();
-                        break;
-                    }
-                case Keys.Space:
-                    break;
-            }
-            Render();
-            return base.ProcessCmdKey(ref msg, keyData);
-        }
 
         public void Verify()
         {
@@ -324,59 +340,44 @@ namespace pac_man
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyData == Keys.Right & hold)
+            if (e.KeyData == Keys.Right )
             {
                 right = true;
+                left = false;
+                up = false;
+                down = false;
                 hold = false;
                 
                 pacman.Image = MyResource.pac_man_d;
             }
-            else if (e.KeyData == Keys.Left & hold)
+            else if (e.KeyData == Keys.Left )
             {
                 left = true;
+                right = false;
+                up = false;
+                down = false;
                 hold = false;
                 pacman.Image = MyResource.pac_man_i;
             }
-            else if (e.KeyData == Keys.Up & hold)
+            else if (e.KeyData == Keys.Up )
             {
                 up = true;
+                right = false;
+                left = false;
+                down = false;
                 hold = false;
                 pacman.Image = MyResource.pac_man_ar;
             }
-            else if (e.KeyData == Keys.Down & hold)
+            else if (e.KeyData == Keys.Down)
             {
                 down = true;
+                right = false;
+                left = false;
+                up = false;
                 hold = false;
                 pacman.Image = MyResource.pac_man_ab;
             }
         }
 
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyData == Keys.Right & !hold)
-            {
-                right = false;
-                hold = true;
-                pacman.Image = MyResource.fijo;
-            }
-            else if (e.KeyData == Keys.Left & !hold)
-            {
-                left = false;
-                hold = true;
-                pacman.Image = MyResource.fijo;
-            }
-            else if (e.KeyData == Keys.Up & !hold)
-            {
-                up = false;
-                hold = true;
-                pacman.Image = MyResource.fijo;
-            }
-            else if (e.KeyData == Keys.Down & !hold)
-            {
-                down = false;
-                hold = true;
-                pacman.Image = MyResource.fijo;
-            }
-        }
     }
 }
